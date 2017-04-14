3e57b23e38e330a6d763a8135004f4b51464da3f
Patrick Karnia <pkarnia_mac@dayabay.iit.edu>
2017-04-14 15:38:01 -0500

    Initial Commit

---
+++ Final/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Final
+//
+//  Created by Patrick Karnia on 4/14/17.
+//  Copyright © 2017 Patrick Karnia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Final/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Final
+//
+//  Created by Patrick Karnia on 4/14/17.
+//  Copyright © 2017 Patrick Karnia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
